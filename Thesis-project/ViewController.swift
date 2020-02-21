3ea1c048694bd3bea5040a4251dc3ba9a0c992ea
Joonas Lauhala <joonas.lauhala@piceasoft.com>
2020-02-21 15:57:53 +0200

    Initial Commit

---
+++ Thesis-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thesis-project
+//
+//  Created by Joonas Lauhala on 21.2.2020.
+//  Copyright © 2020 Joonas Lauhala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Thesis-projectTests/Thesis_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Thesis_projectTests.swift
+//  Thesis-projectTests
+//
+//  Created by Joonas Lauhala on 21.2.2020.
+//  Copyright © 2020 Joonas Lauhala. All rights reserved.
+//
+
+import XCTest
+@testable import Thesis_project
+
+class Thesis_projectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
